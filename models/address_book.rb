d43590990a7bedb6b7ec80b93ab5419a59054543
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-03-27 18:18:23 -0600

    CP7: import from CSV file and make tests

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,35 +1,46 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_reader :entries
 
   def initialize
-     @entries = []
+    @entries = []
   end
 
   def add_entry(name, phone_number, email)
-     # #9
-     index = 0
-     entries.each do |entry|
-     # #10
-       if name < entry.name
-         break
-       end
-       index+= 1
-     end
-     # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
+    # #9
+    index = 0
+    entries.each do |entry|
+      # #10
+      if name < entry.name
+        break
+      end
+      index+= 1
+    end
+    # #11
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
 
-   def remove_entry(name, phone_number, email)
-     entry_to_be_deleted = nil
+  def remove_entry(name, phone_number, email)
+    entry_to_be_deleted = nil
 
-     @entries.each do |entry|
-       if name == entry.name && phone_number == entry.phone_number && email == entry.email
-         entry_to_be_deleted = entry
-       end
-     end
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        entry_to_be_deleted = entry
+      end
+    end
 
-     @entries.delete(entry_to_be_deleted)
-   end
+    @entries.delete(entry_to_be_deleted)
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
 end
