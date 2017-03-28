44021a236dec1a72b642b913a460f5ef3b8ff4f3
Omar Aguinaga <omar.aguinaga94@gmail.com>
2017-03-28 13:21:46 -0600

    A8: iterative serach

--- models/address_book.rb
+++ models/address_book.rb
@@ -68,4 +68,14 @@ class AddressBook
     # #5
     return nil
   end
+
+  def iterative_search(name)
+    @entries.each do |entry|
+      if name == entry.name
+        return entry
+      end
+    end
+    return nil
+  end
+
 end
